4a072cd5d64057124c989a45204db22088ee5653
Sreekumar <sreekumar.y2k@gmail.com>
2021-02-14 15:08:13 +0530

    added comments'

--- javabasics/src/main/java/org/challenges/DriverLambda.java
+++ javabasics/src/main/java/org/challenges/DriverLambda.java
@@ -8,17 +8,22 @@ public class DriverLambda {
 
     public static void main(String[] args) {
 
+        //lambda expression
         Greeter greeterWithLamda = () -> System.out.println("Hello World!");
 
+        //anonymous inner class implementaion
         Greeter greeterWithAnonymous = new Greeter(){
             public void perform(){
                 System.out.println("Hello World!");
             }
         };
 
+        //invoking anonymous inner class instance method
         greeterWithAnonymous.perform();
+        //invoking lambda function
         greeterWithLamda.perform();
 
+        //example of passing anonymous class instance and lambda function as parameter
         DriverLambda driverLambda = new DriverLambda();
         driverLambda.greet(greeterWithAnonymous);
         driverLambda.greet(greeterWithLamda);
