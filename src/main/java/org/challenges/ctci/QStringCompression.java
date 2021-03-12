d6575283c88169ff18aa741b6c328fd426950209
glegshot <sreekumar.y2k@gmail.com>
2021-03-12 21:20:01 +0530

    string compression questions

--- src/main/java/org/challenges/ctci/QStringCompression.java
+++ src/main/java/org/challenges/ctci/QStringCompression.java
@@ -2,6 +2,17 @@ package org.challenges.ctci;
 
 public class QStringCompression {
 
+    /**
+     * solution with linear run time complexity
+     * the solution assumes that there is no whitespaces in the string
+     *
+     * time complexity O(n) where n  is the length of the input string
+     * space complexity O(2n)(worst case) ~ O(n) to store the appended new string
+     *
+     *
+     * @param input
+     * @return String the compressed form of the string
+     */
     public String compressStringSolutionOne(String input) {
 
         StringBuilder outputBuilder = new StringBuilder();
