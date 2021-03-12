691bcbb56ec5429a0d03b6c9a3e222340ab6537b
glegshot <sreekumar.y2k@gmail.com>
2021-03-12 13:39:32 +0530

    added comment

--- src/main/java/org/challenges/ctci/QURLify.java
+++ src/main/java/org/challenges/ctci/QURLify.java
@@ -4,21 +4,21 @@ public class QURLify {
 
 
     /**
-     *
-     * solution using shifting the characters and inserting the encoded character as replacement for space
-     *
+     * solution using shifting the characters and inserting the encoded character as replacement for space.
+     * the solution assumes the input array has exact amount of size required to accommodate the newly encoded values as well.
+     * also the real length of the character array is also provided.
+     * <p>
      * time complexity O(n+2m) ~ O(n+m) where n = length of the input string, m = number of whitespace characters(each whitespace will be replaced with three characters)
      * space complexity O(n+2m) ~ O(n+m)
      *
-     *
-     * @param input
-     * @param trueLength
+     * @param input      character array of input string
+     * @param trueLength real length of the character array
      * @return char[] output char array of encoded value
      */
     public char[] performURLifyByShifting(char[] input, int trueLength) {
 
-        int i = trueLength-1;
-        int j = input.length-1;
+        int i = trueLength - 1;
+        int j = input.length - 1;
         char[] encodeArray = new char[]{'%', '2', '0'};
 
         while (i >= 0) {
