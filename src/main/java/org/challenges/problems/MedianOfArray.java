a6b11dae1548113fb39d601344ddf2bab8b9eac5
glegshot <sreekumar.y2k@gmail.com>
2021-03-13 17:50:34 +0530

    median of array quadratic runtime solution

--- src/main/java/org/challenges/problems/MedianOfArray.java
+++ src/main/java/org/challenges/problems/MedianOfArray.java
@@ -1,4 +1,54 @@
 package org.challenges.problems;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MedianOfArray {
+
+    /**
+     * Solution with traversing the array n/2 + 1 times to get the median of the array
+     * The solution assumes the elements in the array are larger than Integer.MIN_VALUE and no repeated elements
+     * <p>
+     * time complexity O(n*n)
+     * space complexity O(1)
+     *
+     * @param valueArray
+     * @return int[] median values
+     */
+    public List<Integer> findMedian(@org.jetbrains.annotations.NotNull int[] valueArray) {
+
+        //variable to hold current max value in the iteration
+        int max = Integer.MIN_VALUE;
+
+        //variable to hold the previous iteration max value
+        int prevMax = Integer.MAX_VALUE;
+
+        int iterationRequired = 0;
+        boolean isEven = true;
+
+        List<Integer> median = new ArrayList<>();
+
+        if (valueArray.length % 2 == 0) {
+            iterationRequired = valueArray.length/2 + 1;
+        } else {
+            iterationRequired = valueArray.length/2;
+            isEven = false;
+        }
+
+        for (int counter = 0; counter < iterationRequired; counter++) {
+            max = Integer.MIN_VALUE;
+            for (int i = 0; i < valueArray.length; i++) {
+                if (valueArray[i] >= max && valueArray[i] < prevMax) {
+                    max = valueArray[i];
+                }
+            }
+            if (counter == iterationRequired - 1 && isEven) {
+                median.add(max);
+            }
+            prevMax = max;
+        }
+        median.add(prevMax);
+        return median;
+    }
+
 }
--- src/test/java/org/challenges/problems/MedianOfArrayTest.java
+++ src/test/java/org/challenges/problems/MedianOfArrayTest.java
@@ -1,2 +1,34 @@
-package org.challenges.problems;public class MedianOfArrayTest {
+package org.challenges.problems;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RunWith(JUnit4.class)
+public class MedianOfArrayTest {
+
+    MedianOfArray medianOfArray;
+
+    @Before
+    public void init() {
+        medianOfArray = new MedianOfArray();
+    }
+
+    @Test
+    public void medianOfArrayOfNumbersNaiveApproachTest() {
+        List<Integer> expectedMedian = new ArrayList<>();
+        expectedMedian.add(8);
+        int[] oddArray = new int[]{8, 5, 16, 21, 2, 4, 3};
+        List<Integer> actualResult;
+        actualResult = medianOfArray.findMedian(oddArray);
+        Assert.assertSame(expectedMedian.get(0), actualResult.get(0));
+        Assert.assertEquals(1, actualResult.size());
+
+    }
+
 }
