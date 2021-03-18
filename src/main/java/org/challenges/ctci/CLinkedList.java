8694f1aec5bc1f5916303c0213cf1ef7e5379fd1
glegshot <sreekumar.y2k@gmail.com>
2021-03-18 12:05:07 +0530

    linked list implementation

--- src/main/java/org/challenges/ctci/CLinkedList.java
+++ src/main/java/org/challenges/ctci/CLinkedList.java
@@ -1,2 +1,103 @@
-package org.challenges.ctci;public class CLinkedList {
+package org.challenges.ctci;
+
+/**
+ * Singly Linked List Implementation
+ * The implementation includes add,get,remove operations
+ */
+public class CLinkedList<T> {
+
+    private Node<T> head;
+    private int listSize;
+
+    public CLinkedList(T value) {
+        this.head = new Node<>(value);
+        this.listSize += 1;
+    }
+
+    /**
+     * add method to insert a value to end of the list
+     * <p>
+     * time complexity O(n) where n is the length of the list
+     *
+     * @param value
+     */
+    public void add(T value) {
+        Node n = this.head;
+        while (n.next != null) {
+            n = n.next;
+        }
+        n.next = new Node(value);
+        this.listSize += 1;
+    }
+
+    /**
+     * get method to get the value at a particular position starting with index 0
+     * <p>
+     * time complexity O(n) where n is length of the list
+     * if the value not present in the list then it will iterate for the n nodes and return a null
+     *
+     * @param index
+     * @return
+     */
+    public T get(int index) {
+        int counter = 0;
+        Node n = this.head;
+        while (n != null && counter < index) {
+            n = n.next;
+            if (counter == index) {
+                return (T) n.value;
+            }
+            counter++;
+        }
+
+        return null;
+
+    }
+
+    /**
+     * remove operation to remove a value
+     * <p>
+     * time complexity is O(n) where n is lenght of the list
+     * <p>
+     * if the item not present then returns a null
+     *
+     * @param value
+     * @return T removed node value
+     */
+    public T remove(T value) {
+        Node n = this.head;
+        T v = null;
+        if (n.value == value) {
+            v = (T) n.value;
+        }
+
+        while (n.next != null) {
+            if (n.next.value == value) {
+                v = (T) n.next.value;
+                n.next = n.next.next;
+            }
+        }
+        return v;
+    }
+
+    public Node<T> getHead() {
+        return head;
+    }
+
+    public int getListSize() {
+        return listSize;
+    }
+
+    private static class Node<T> {
+
+        T value;
+        Node next;
+
+        public Node(T value) {
+            this.value = value;
+            this.next = null;
+        }
+
+    }
+
 }
--- src/test/java/org/challenges/ctci/CLinkedListTest.java
+++ src/test/java/org/challenges/ctci/CLinkedListTest.java
@@ -1,2 +1,55 @@
-package org.challenges.ctci;public class CLinkedListTest {
+package org.challenges.ctci;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+
+public class CLinkedListTest {
+
+    CLinkedList<Integer> linkedList;
+
+    @Before
+    public void init(){
+        linkedList = new CLinkedList(92);
+    }
+
+    @Test
+    public void linkedListInsertTest(){
+        linkedList.add(25);
+        linkedList.add(12);
+        linkedList.add(32);
+        Assert.assertEquals(4,linkedList.getListSize());
+    }
+
+    @Test
+    public void linkedListGetValueTest(){
+        int value = linkedList.get(0);
+        Assert.assertEquals(92, value);
+    }
+
+
+    @Test
+    public void linkedListGetValueNullTest(){
+        Integer value = linkedList.get(1);
+        Assert.assertEquals(null, value);
+    }
+
+
+    @Test
+    public void linkedListRemoveValueTest(){
+        Integer value = linkedList.remove(92);
+        Assert.assertNotNull(value);
+
+    }
+
+    @Test
+    public void linkedListRemoveValueNullTest(){
+        Integer value = linkedList.remove(23);
+        Assert.assertNull(value);
+
+    }
+
+
 }
