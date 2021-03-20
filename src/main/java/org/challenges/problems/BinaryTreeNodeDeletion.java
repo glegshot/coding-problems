098d5ef3d8de11d4bd44cc8331f9b056d4a1f51e
glegshot <sreekumar.y2k@gmail.com>
2021-03-20 16:37:02 +0530

    binary tree deletion questions GFG

---
+++ src/main/java/org/challenges/problems/BinaryTreeNodeDeletion.java
@@ -0,0 +1,85 @@
+package org.challenges.problems;
+
+public class BinaryTreeNodeDeletion {
+
+
+    int depth = 0;
+    BinaryTreeNode n;
+    BinaryTreeNode delNode;
+
+
+    /**
+     *
+     * method to delete a node value in binary tree
+     *
+     * time complexity O(n) where n is the number of nodes in the tree
+     * space complexity O(1) ;ignoring the call stacks
+     *
+     * the solution recursively searches the tree for the node with designated delete value and the bottom-right most node
+     * and replaced the delete value node with bottom-right most value
+     *
+     *
+     */
+    public boolean deleteNode(BinaryTreeNode root, int deleteValue) {
+        n = root;
+        delNode = root;
+        findNodes(root, deleteValue, 0);
+
+        if (n.rightChild != null) {
+            delNode.data = n.rightChild.data;
+            n.rightChild = null;
+            return true;
+        } else if (n.leftChild != null) {
+            delNode.data = n.leftChild.data;
+            n.leftChild = null;
+            return true;
+        }
+
+        return false;
+
+    }
+
+    public void findNodes(BinaryTreeNode root, int deleteValue, int count) {
+        if(root == null){
+            return;
+        }
+
+        if ((root.rightChild != null) && (root.rightChild.data == deleteValue)) {
+            this.delNode = root.rightChild;
+        } else if ((root.leftChild != null) && (root.leftChild.data == deleteValue)) {
+            this.delNode = root.leftChild;
+        }
+
+        if (root.rightChild != null) {
+            if (count > this.depth) {
+                this.depth = count;
+                this.n = root;
+            }
+            findNodes(root.rightChild, deleteValue, count + 1);
+
+        }
+        if (root.leftChild != null) {
+            if (count > this.depth) {
+                this.depth = count;
+                this.n = root;
+            }
+            findNodes(root.leftChild, deleteValue, count + 1);
+
+        }
+
+    }
+
+
+    public static class BinaryTreeNode {
+
+        int data;
+        BinaryTreeNode leftChild;
+        BinaryTreeNode rightChild;
+
+        public BinaryTreeNode(int data) {
+            this.data = data;
+        }
+    }
+
+
+}
---
+++ src/test/java/org/challenges/problems/BinaryTreeNodeDeletionTest.java
@@ -0,0 +1,110 @@
+package org.challenges.problems;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+@RunWith(JUnit4.class)
+public class BinaryTreeNodeDeletionTest {
+
+    @Test
+    public void simpleBinaryTreeDeletionTest() {
+
+        BinaryTreeNodeDeletion.BinaryTreeNode root = new BinaryTreeNodeDeletion.BinaryTreeNode(10);
+        BinaryTreeNodeDeletion.BinaryTreeNode leftOne = new BinaryTreeNodeDeletion.BinaryTreeNode(20);
+        BinaryTreeNodeDeletion.BinaryTreeNode rightOne = new BinaryTreeNodeDeletion.BinaryTreeNode(30);
+        root.leftChild = leftOne;
+        root.rightChild = rightOne;
+
+        List<Integer> expectedList = new ArrayList<>();
+        expectedList.add(30);
+        expectedList.add(20);
+        expectedList.add(null);
+        expectedList.add(null);
+        expectedList.add(null);
+
+        BinaryTreeNodeDeletion binaryTreeNodeDeletion = new BinaryTreeNodeDeletion();
+        Assert.assertTrue(binaryTreeNodeDeletion.deleteNode(root, 10));
+
+        Assert.assertTrue(expectedList.equals(generateResult(root)));
+
+    }
+
+    @Test
+    public void SecondExampleBinaryTreeDeletionTest() {
+
+        BinaryTreeNodeDeletion.BinaryTreeNode root = new BinaryTreeNodeDeletion.BinaryTreeNode(10);
+        BinaryTreeNodeDeletion.BinaryTreeNode leftOne = new BinaryTreeNodeDeletion.BinaryTreeNode(20);
+        BinaryTreeNodeDeletion.BinaryTreeNode rightOne = new BinaryTreeNodeDeletion.BinaryTreeNode(40);
+        BinaryTreeNodeDeletion.BinaryTreeNode leftTwo = new BinaryTreeNodeDeletion.BinaryTreeNode(60);
+        BinaryTreeNodeDeletion.BinaryTreeNode leftThree = new BinaryTreeNodeDeletion.BinaryTreeNode(70);
+        BinaryTreeNodeDeletion.BinaryTreeNode rightThree = new BinaryTreeNodeDeletion.BinaryTreeNode(100);
+        BinaryTreeNodeDeletion.BinaryTreeNode leftFour = new BinaryTreeNodeDeletion.BinaryTreeNode(25);
+
+        root.leftChild = leftOne;
+        root.rightChild = rightOne;
+        root.leftChild.leftChild = leftTwo;
+        root.leftChild.leftChild.leftChild = leftThree;
+        root.leftChild.leftChild.rightChild = rightThree;
+        root.leftChild.leftChild.leftChild.leftChild = leftFour;
+
+        List<Integer> expectedList = new ArrayList<>();
+        expectedList.add(10);
+        expectedList.add(25);
+        expectedList.add(40);
+        expectedList.add(60);
+        expectedList.add(null);
+        expectedList.add(null);
+        expectedList.add(null);
+        expectedList.add(70);
+        expectedList.add(100);
+        expectedList.add(null);
+        expectedList.add(null);
+        expectedList.add(null);
+        expectedList.add(null);
+
+        BinaryTreeNodeDeletion binaryTreeNodeDeletion = new BinaryTreeNodeDeletion();
+        Assert.assertTrue(binaryTreeNodeDeletion.deleteNode(root, 20));
+
+
+        Assert.assertTrue(expectedList.equals(generateResult(root)));
+
+    }
+
+
+
+    /**
+     * this method will generate a perfect binary tree list
+     *
+     * @param root
+     * @return
+     */
+    public List<Integer> generateResult(BinaryTreeNodeDeletion.BinaryTreeNode root) {
+        List<Integer> resultList = new ArrayList<>();
+        Queue<BinaryTreeNodeDeletion.BinaryTreeNode> queue = new LinkedList<>();
+
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            BinaryTreeNodeDeletion.BinaryTreeNode node = queue.poll();
+            if (node != null) {
+                queue.add(node.leftChild);
+                queue.add(node.rightChild);
+                resultList.add(node.data);
+            } else {
+                resultList.add(null);
+            }
+
+        }
+
+        return resultList;
+    }
+
+
+}
